283b14ca55ab0ed73c51e44544c723078be30e0c
usamahassankhan <usamahassankhan870@gmail.com>
2020-11-05 23:06:27 +0500

    nice

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,7 @@
 const initState={
 projects:[
-    {id:'1',title:'help0 me0 find0 peach0',content:'blah blah blah'},
-    {id:'2',title:'help1 me1 find1 peach1',content:'blah1 blah1 blah1'},
+    {id:'1',title:'help me bro',content:'blah blah blah'},
+    {id:'2',title:'dnt help me',content:'blah1 blah1 blah1'},
     {id:'3',title:'help2 me2 find2 peach2',content:'blah2 blah2 blah2'}
 ]
 }
