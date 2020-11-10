a553c64dc83a8e859bbd814cb577fca716adc8f3
usamahassankhan <usamahassankhan870@gmail.com>
2020-11-10 08:24:15 +0500

    duhur

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -2,11 +2,15 @@ const initState={
 projects:[
     {id:'1',title:'help me bro',content:'blah blah blah'},
     {id:'2',title:'dnt help me',content:'blah1 blah1 blah1'},
-    {id:'3',title:'help2 me2 find2 peach2',content:'blah2 blah2 blah2'}
+    {id:'3',title:'collect all the stars',content:'blah2 blah2 blah2'}
 ]
 }
 
 const projectReducer=(state=initState,action)=>{
-return state
+    switch(action.type){
+        case 'CREATE_PROJECT':
+            console.log("createdproject",action.project)
+    }
+    return state
 }
 export default projectReducer
